df9eed52b722ba8295e82676015382b43c176c8e
CLP <a2017@2017s-MacBook-Pro.local>
2025-09-20 09:05:42 +0700

    adding email service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,11 +3,12 @@ import { AppService } from './app.service';
 import { JwtAuthGuard } from './modules/auth/guards/logged-in.guard';
 import { PermissionGuard } from './modules/auth/guards/permission.guard';
 import { RequiredAnyPermissions } from './modules/auth/decorators/permissions.decorator';
+import { EmailService } from './common/email/email.services';
 
 @Controller()
 @UseGuards(JwtAuthGuard, PermissionGuard)
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly emailService: EmailService) {}
 
   @Get()
   getHello(): string {
@@ -18,5 +19,11 @@ export class AppController {
   @RequiredAnyPermissions('shipments.create')
   getProtectedResource():string{
     return 'this is a protected resourcee'
+  }
+
+  @Get('send-email-test')
+  async sendTestEmail():Promise<string>{
+    await this.emailService.testingEmail('testing@gmail.com');
+    return 'Test email sent successfully';
   }
 }
---
+++ src/common/email/email.services.ts
@@ -0,0 +1,56 @@
+import { Injectable } from "@nestjs/common";
+import * as nodemailer from "nodemailer";
+import path from "path";
+
+@Injectable()
+export class EmailService {
+    private transporter: nodemailer.Transporter;
+    private templatePath:string
+
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: parseInt(process.env.SMTP_PORT||'25'),
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD,
+            },
+        })
+
+        this.templatePath = path.join('.src/common/email/templates')
+    }
+
+    private loadTemplate(templateName: string): string {
+        const templatePath = path.join(this.templatePath, `${templateName}.html`);
+        return require('fs').readFileSync(templatePath, 'utf-8');
+    }
+
+    private compileTemplate(
+        templateName: string,
+        data:any,
+    ):string{
+        const templateSource = this.loadTemplate(templateName);
+        const template = require('handlebars').compile(templateSource);
+        return template(data);
+    }
+
+    async testingEmail(to:string):Promise<void>{
+        const templateData = {
+            title: 'Test Email',
+            message: 'This is a test email from our application'
+        }
+        const htmlContent = this.compileTemplate('test-email', templateData);
+        
+        const mailOptions = {
+            from: process.env.SMTP_EMAIL_SENDER,
+            to,
+            subject: 'Test Email KirimAja(no need reply)',
+            html: htmlContent,
+        }
+
+        await this.transporter.sendMail(mailOptions);
+    }
+}
+
+
